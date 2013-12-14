f815b5b5ae6dfd05cf1cf5983af505fd2028f2f8
Kuber Sodari <kuber2009@hotmail.com>
2013-12-14 09:21:11 -0800

    Update 1.1lab.cpp

--- 1.1lab.cpp
+++ 1.1lab.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     float far,cel;
 
 cout<<"Enter temperature in Fahrenheit to convert into Celsius"<<endl;
 cin>>far;
 cel=5*(far-32)/9;
-cout<<far<<" Degree Farenheit = "<<cel<<" Degree Celcius.";
+cout<<far<<" Farenheit = "<<cel<<" Degree Celcius.";
 
 return 1;
 }
